9ea01536554e4d5ed29330df50fca9005b939d84
Brandon <25nguyenb26@stu.smuhsd.org>
2023-06-29 11:39:39 -0700

    Adding colors to the toolbar and correct props
    Now, react components can be added as children, as well as having correct colors

--- Components/Tools/Toolbar.tsx
+++ Components/Tools/Toolbar.tsx
@@ -3,11 +3,11 @@ import { FC } from "react";
 export type Tool = {};
 
 type ToolbarProps = {
-	children: Tool;
+	children: React.ReactNode;
 };
 
 const Toolbar: FC<ToolbarProps> = ({ children }): JSX.Element => {
-	return <div className={"toolbar"}>{children}</div>;
+	return <div className={"toolbar primary"}>{children}</div>;
 };
 
 export default Toolbar;
