0dd5eb23c3f86a3223de7aa33676afc5195c1c5a
Brandon <25nguyenb26@stu.smuhsd.org>
2023-07-03 17:20:33 -0700

    Updates to style
    Figuring out tailwind as i go lmao

--- Components/Tools/Toolbar.tsx
+++ Components/Tools/Toolbar.tsx
@@ -6,8 +6,13 @@ type ToolbarProps = {
 	children: React.ReactNode;
 };
 
+// Storing all the styles in a cleaner way than raw text lol
+const styles = ["bg-primary", "max-w-sm", "mx-auto", "flex", "justify-evenly"].reduce(
+    (prevStyle, currentStyle) => prevStyle + " " + currentStyle
+)
+
 const Toolbar: FC<ToolbarProps> = ({ children }): JSX.Element => {
-	return <div className={"toolbar primary"}>{children}</div>;
+	return <div className={`toolbar ${styles}`}>{children}</div>;
 };
 
 export default Toolbar;
